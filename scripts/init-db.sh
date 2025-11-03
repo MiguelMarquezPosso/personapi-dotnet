0102a3ad485caa619e432fcf41bf79443cb98e4d
Adrian Ruiz <adrianruiz0208@gmail.com>
2025-11-03 16:35:11 -0500

    Deploy: OK

--- scripts/init-db.sh
+++
@@ -1,9 +0,0 @@
-﻿#!/bin/bash
-# Wait for SQL Server to be ready
-echo "Waiting for SQL Server to start..."
-sleep 30s
-
-echo "Running database initialization script..."
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "Arqui2025!" -d master -i /docker-entrypoint-initdb.d/init-db.sql
-
-echo "Database initialization completed!"
